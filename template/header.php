4898124db68d8b6b3e55f575519122b0f20d4e75
Zenon <veresovdanil@gmail.com>
2024-12-06 21:04:42 +0300

    html разметка хэдера

---
+++ template/header.php
@@ -0,0 +1,65 @@
+<?php
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Главная</title>
+    <link rel="stylesheet" href="../style/main.css">
+</head>
+
+<body>
+<header class="header">
+    <div class="header-wrapper">
+        <div class="header__top">
+            <div class="header__logo">
+                <img src="../image/header/logo.svg" alt="">
+            </div>
+            <div class="catalog__button">
+                <span class="catalog__button_text">Каталог</span>
+                <div class="catalog__button_burger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <form action="" class="header-">
+                <input type="search">
+            </form>
+            <div class="header__controls">
+                <a class="class=" header__controls_favourites" href="">
+                <img src="" alt=""><span>Избранное</span>
+                </a>
+                <a class="header__controls_cart" href="">
+                    <img src="" alt=""><span>Корзина</span>
+                </a>
+                <div class="header__controls_sigin">
+                    <button>Войти</button>
+                </div>
+            </div>
+        </div>
+        <div class="header__tags">
+            <ul>
+                <a href="">Все</a>
+                <a href="">Фэнтази</a>
+                <a href="">Детективы</a>
+                <a href="">Боевики</a>
+                <a href="">Проза</a>
+                <a href="">Бизнес</a>
+                <a href="">Изотерика</a>
+                <a href="">Психология</a>
+                <a href="">Мода</a>
+                <a href="">Кулинария</a>
+                <a href="">Биография</a>
+                <a href="">Хобби</a>
+                <a href="">Домашние животные</a>
+            </ul>
+        </div>
+    </div>
+</header>
