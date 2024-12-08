ac836f4ca7ecd506eebc07eedd7d36f24d81776b
Zenon <veresovdanil@gmail.com>
2024-12-08 13:31:05 +0300

    feat: начал верстку строки поиска в хэдере

--- template/header.php
+++ template/header.php
@@ -1,6 +1,5 @@
 <?php
 
-
 ?>
 
 <!doctype html>
@@ -11,26 +10,33 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Главная</title>
+
     <link rel="stylesheet" href="../style/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&family=Philosopher:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
 <header class="header">
     <div class="header-wrapper">
-        <div class="header__top">
-            <div class="header__logo">
-                <img src="../image/header/logo.svg" alt="">
-            </div>
-            <div class="catalog__button">
-                <span class="catalog__button_text">Каталог</span>
-                <div class="catalog__button_burger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+        <div class="header-top">
+            <div class="header-top__logo-position">
+                <div class="header__logo">
+                    <img src="../image/header/logo.svg" alt="">
                 </div>
+                <a class="catalog__button-link" href="">
+                    <div class="catalog__button">
+                        <span class="catalog__button_text">Каталог</span>
+                        <div class="catalog__button-elem">
+                            <span class="catalog__button-elem-item"></span>
+                        </div>
+                    </div>
+                </a>
             </div>
-            <form action="" class="header-">
-                <input type="search">
+            <form action="" class="header-search-form">
+                <button><img src="../image/header/Search_Small.svg" alt=""></button>
+                <input placeholder="Поиск" class="header-search" type="text">
             </form>
             <div class="header__controls">
                 <a class="class=" header__controls_favourites" href="">
